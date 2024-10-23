afa12eed2b50573076066482275f46ed783c9ab6
Anderson Lozano <adslozanov@gmail.com>
2024-10-23 13:07:03 -0500

    :lipstick: Colors are added and configured in tailwind.config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,23 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				colors: {
+					'lightest': 'var(--color-lightest)',
+					'lighter': 'var(--color-lighter)',
+					'light': 'var(--color-light)',
+					'medium-light': 'var(--color-medium-light)',
+					'medium': 'var(--color-medium)',
+					'medium-dark': 'var(--color-medium-dark)',
+					'dark': 'var(--color-dark)',
+					'darker': 'var(--color-darker)',
+					'darkest': 'var(--color-darkest)',
+					'almost-black': 'var(--color-almost-black)',
+					'black': 'var(--color-black)',
+				},
+			}
+		},
 	},
 	plugins: [],
 }
