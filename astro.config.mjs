72e4f591ce4b9c32c49c66ed7aa1305ced54751d
Anderson Lozano <adslozanov@gmail.com>
2024-12-10 17:31:38 -0500

    :rocket: Preparing the site for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  base: '/RC-Dental',
+  build: {
+    format: 'directory',
+    client: 'dist/client',
+    server: 'dist/server',
+    assets: 'dist/assets'
+  },
   integrations: [tailwind()],
   i18n: {
     defaultLocale: 'es',
